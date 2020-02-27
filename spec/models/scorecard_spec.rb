38a94e608e9c7ec184c18d5c09a5d3cf3386420a
Tylor Schafer <tylorschafer@gmail.com>
2020-02-26 22:05:01 -0700

    Add Scorecard helper method testing

--- spec/models/scorecard_spec.rb
+++ spec/models/scorecard_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'Scorecard' do
 
   before :each do
     @scorecard = Scorecard.new()
+    @dice = []
+    6.times do
+      @dice << Die.new()
+    end
   end
 
   describe 'Instantiation' do
@@ -35,6 +39,39 @@ RSpec.describe 'Scorecard' do
         expect(@scorecard.lower_section.lg_straight).to eq(0)
         expect(@scorecard.lower_section.yahtzee).to eq(0)
         expect(@scorecard.lower_section.chance).to eq(0)
+      end
+    end
+  end
+
+  describe 'Instance m\Methods' do
+    describe 'Helper Methods' do
+      before :each do
+        @dice[0].curr_value = 1
+        @dice[1].curr_value = 1
+        @dice[2].curr_value = 1
+        @dice[3].curr_value = 2
+        @dice[4].curr_value = 3
+        @dice[5].curr_value = 5
+      end
+      it 'group_finder() finds the count of each die number in play' do
+        expect(@scorecard.group_finder(@dice)).to eq({
+                                                      1 => 3,
+                                                      2 => 1,
+                                                      3 => 1,
+                                                      5 => 1
+                                                    })
+      end
+      it 'seq_finder() returns the longest sequence length of the die in play' do
+         expect(@scorecard.seq_finder(@dice)).to eq(3)
+
+         @dice[0].curr_value = 1
+         @dice[1].curr_value = 2
+         @dice[2].curr_value = 3
+         @dice[3].curr_value = 4
+         @dice[4].curr_value = 5
+         @dice[5].curr_value = 1
+
+         expect(@scorecard.seq_finder(@dice)).to eq(5)
       end
     end
   end

b746eb4125bc81416040d1f3e2d0db837dc43623
Tylor Schafer <tylorschafer@gmail.com>
2020-02-26 22:39:27 -0700

    Add upper and lower_section attributes

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -3,5 +3,22 @@ class Scorecard
 
   def initialize()
     @total_score = 0
+    @upper_section = {
+      aces: 0,
+      twos: 0,
+      threes: 0,
+      fours: 0,
+      fives: 0,
+      sixes: 0,
+    }
+    @lower_section = {
+      three_of_kind: 0,
+      four_of_kind: 0,
+      full_house: 0,
+      sm_straight: 0,
+      lg_straight: 0,
+      yahtzee: 0,
+      chance: 0
+    }
   end
 end
--- spec/models/scorecard_spec.rb
+++ spec/models/scorecard_spec.rb
@@ -20,25 +20,25 @@ RSpec.describe 'Scorecard' do
         expect(@scorecard.total_score).to eq(0)
       end
 
-      xit '@upper_section is a hash containing all upper section scores' do
+      it '@upper_section is a hash containing all upper section scores' do
         expect(@scorecard.upper_section).to be_an_instance_of(Hash)
-        expect(@scorecard.upper_section.aces).to eq(0)
-        expect(@scorecard.upper_section.twos).to eq(0)
-        expect(@scorecard.upper_section.threes).to eq(0)
-        expect(@scorecard.upper_section.fours).to eq(0)
-        expect(@scorecard.upper_section.fives).to eq(0)
-        expect(@scorecard.upper_section.sixes).to eq(0)
+        expect(@scorecard.upper_section[:aces]).to eq(0)
+        expect(@scorecard.upper_section[:twos]).to eq(0)
+        expect(@scorecard.upper_section[:threes]).to eq(0)
+        expect(@scorecard.upper_section[:fours]).to eq(0)
+        expect(@scorecard.upper_section[:fives]).to eq(0)
+        expect(@scorecard.upper_section[:sixes]).to eq(0)
       end
 
-      xit '@lower_section is a hash containing all lower section scores' do
+      it '@lower_section is a hash containing all lower section scores' do
         expect(@scorecard.lower_section).to be_an_instance_of(Hash)
-        expect(@scorecard.lower_section.three_of_kind).to eq(0)
-        expect(@scorecard.lower_section.four_of_kind).to eq(0)
-        expect(@scorecard.lower_section.full_house).to eq(0)
-        expect(@scorecard.lower_section.sm_straight).to eq(0)
-        expect(@scorecard.lower_section.lg_straight).to eq(0)
-        expect(@scorecard.lower_section.yahtzee).to eq(0)
-        expect(@scorecard.lower_section.chance).to eq(0)
+        expect(@scorecard.lower_section[:three_of_kind]).to eq(0)
+        expect(@scorecard.lower_section[:four_of_kind]).to eq(0)
+        expect(@scorecard.lower_section[:full_house]).to eq(0)
+        expect(@scorecard.lower_section[:sm_straight]).to eq(0)
+        expect(@scorecard.lower_section[:lg_straight]).to eq(0)
+        expect(@scorecard.lower_section[:yahtzee]).to eq(0)
+        expect(@scorecard.lower_section[:chance]).to eq(0)
       end
     end
   end

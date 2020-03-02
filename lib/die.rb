133bb12a82c7ba793ab363f8cbba531fd4c1228d
Tylor Schafer <tylorschafer@gmail.com>
2020-03-01 20:28:03 -0700

    Add working Player load() functionality

--- lib/die.rb
+++ lib/die.rb
@@ -1,10 +1,11 @@
 class Die
   attr_reader :values
-  attr_accessor :curr_value
+  attr_accessor :curr_value, :number
 
-  def initialize()
+  def initialize(number = 0)
     @values = [1,2,3,4,5,6]
     @curr_value = nil
+    @number = number
   end
 
   def roll()
--- lib/player.rb
+++ lib/player.rb
@@ -10,15 +10,23 @@ class Player
     @in_play = []
     @cup = Cup.new()
     @scorecard = Scorecard.new()
+    @die_count = 1
     6.times do
-      @cup.contents << Die.new()
+      @cup.contents << Die.new(@die_count)
+      @die_count += 1
     end
   end
 
   def load()
     prompt = TTY::Prompt.new
-    dice = @in_play.map { |die| "| #{die.curr_value} |" }
+    dice = @in_play.map { |die| "Die ##{die.number} | #{die.curr_value} |" }
     dice << "I don't want to load any dice"
-    prompt.multi_select('What Dice would you like to Load?', dice)
+    selection = prompt.multi_select('What Dice would you like to Load?', dice)
+    user_selection = selection.map { |die| die[5].to_i }
+    user_selection.each do |selection|
+      die = @in_play.find { |die| die.number == selection }
+      @cup.contents << die
+      @in_play.delete(die)
+    end
   end
 end

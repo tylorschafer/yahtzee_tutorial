52c968dfbb519442d96335592289b872be08cc31
Tylor Schafer <tylorschafer@gmail.com>
2020-03-11 16:15:50 -0600

    Add find_unscored_options helper method to help in score() method

--- lib/player.rb
+++ lib/player.rb
@@ -43,49 +43,14 @@ class Player
 
   def score()
     prompt = TTY::Prompt.new
-    selections = [@scorecard.upper_section.keys]
-    selections.push(*@scorecard.lower_section.keys)
+    selections = find_unscored_options()
+    binding.pry
     selection = prompt.select('How would you like to score your dice?', selections)
-    case selection
-    when :aces
-      score = @scorecard.tally_die(@in_play, 1, :aces)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :twos
-      score = @scorecard.tally_die(@in_play, 2, :twos)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :threes
-      score = @scorecard.tally_die(@in_play, 3, :threes)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :fours
-      score = @scorecard.tally_die(@in_play, 4, :fours)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :fives
-      score = @scorecard.tally_die(@in_play, 5, :fives)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :sixes
-      score = @scorecard.tally_die(@in_play, 6, :sixes)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :full_house
-      score = @scorecard.full_house(@in_play)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :three_of_kind
-      score = @scorecard.of_kind(@in_play, 3, :three_of_kind)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :four_of_kind
-      score = @scorecard.of_kind(@in_play, 4, :four_of_kind)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :sm_straight
-      score = @scorecard.straight(@in_play, 4, :sm_straight)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :lg_straight
-      score = @scorecard.straight(@in_play, 5, :lg_straight)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :yahtzee
-      score = @scorecard.yahtzee(@in_play)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    when :chance
-      valid = @scorecard.chance(@in_play)
-      p "#{selection.to_s.tr('_', ' ')} has been scored for #{score} point!"
-    end
+  end
+
+  def find_unscored_options()
+    upper = @scorecard.upper_section.find_all { |key, score| score == 0 }.map{|k,v| k}
+    lower = @scorecard.lower_section.find_all { |key, score| score == 0 }.map{|k,v| k}
+    upper + lower
   end
 end
